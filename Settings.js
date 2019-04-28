7f846be2ca67a7c206ae087791ac6946f6d874b5
Sergiusz Mazan <mazan.sergiusz@gmail.com>
2019-04-29 01:17:39 +0200

    Add saving server address

--- Settings.js
+++ Settings.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import  AsyncStorage from '@react-native-community/async-storage';
 import { SettingHeader, SettingComponent } from './SettingElements';
 import DialogInput from "react-native-dialog-input";
 
@@ -15,8 +16,32 @@ export default class SettingsComponent extends Component {
 
   state = {
     dialogVisible: false,
-    address: "192.168.1.9"
+    address: null
   }
+
+  componentDidMount() {
+    this.readServerIP();
+  }
+
+  readServerIP = async () => {
+    try {
+        const val = await AsyncStorage.getItem("address");
+        if (val != null) {
+            this.setState({address: val});
+            AsyncStorage.flushGetRequests();
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+  };
+
+  saveServerIP = async (address) => {
+    try {
+        AsyncStorage.setItem("address", address);
+    } catch (error) {
+        alert(error.message);
+    }
+  };
 
   render() {
 
@@ -29,23 +54,25 @@ export default class SettingsComponent extends Component {
                           iconName='md-at'
                           iconType='ionicon'/>
 
-          <DialogInput isDialogVisible={this.state.dialogVisible}
-                      title={"Change server address"}
-                      message={"Please enter new address of the server"}
-                      initValueTextInput={this.state.address}
-                      submitInput={ (inputText) => { this.handleAddressSubmit(inputText) } }
-                      closeDialog={ () => {this.handleAddressCancel()}}>
-          </DialogInput>
+        <DialogInput isDialogVisible={this.state.dialogVisible}
+                     title={"Change server address"}
+                     message={"Please enter new address of the server"}
+                     initValueTextInput={this.state.address}
+                     submitInput={ (inputText) => { this.handleAddressSubmit(inputText) } }
+                     closeDialog={ () => {this.handleAddressCancel()}}>
+        </DialogInput>
       </View>
     );
   }
 
   showAddressDialog = () => {
+    this.readServerIP(); 
     this.setState({ dialogVisible: true});
   }
 
   handleAddressSubmit = (newAddress) => {
-    this.setState({ address: newAddress})
+    this.setState({ address: newAddress});
+    this.saveServerIP(newAddress);
     this.setState({ dialogVisible: false});
   }
 
